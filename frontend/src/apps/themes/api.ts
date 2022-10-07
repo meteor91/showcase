06616e9004984c7ed842e4eb99248b3e46b9f8ad
meteor91 <bilalsir@yandex.ru>
2022-10-07 17:17:14 +0500

    added Breadcrumbs, rename questions module to themes

---
+++ frontend/src/apps/themes/api.ts
@@ -0,0 +1,19 @@
+import { http, post, get, put } from 'core/http';
+import { IPaginatedData } from 'core/models';
+import { defaultPageSize } from 'core/consts';
+import { IQuestion, ITheme } from './models';
+
+export const getQuestionsList = (page: number = 0) => http.get<IPaginatedData<IQuestion>>(
+    `/questions/questions/?limit=${defaultPageSize}${page ? `&offset=${defaultPageSize*(page-1)}`: ''}`
+);
+
+export const getThemesList = (page: number = 0) => http.get<IPaginatedData<ITheme>>(
+    `/questions/themes/?limit=${defaultPageSize}${page ? `&offset=${defaultPageSize*(page-1)}`: ''}`
+);
+
+export const createTheme = (theme: ITheme) => post<ITheme>('/questions/themes/', theme);
+
+export const editTheme = (theme: ITheme, id: string) => put<ITheme>(`/questions/themes/${id}/`, theme);
+
+//TODO: разобраться с типизацией
+export const getTheme = (id: any) => get<ITheme>(`/questions/themes/${id}/`);
