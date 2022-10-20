fa78d42f4c6adb7a2a7b8d014e970efdc5731488
meteor91 <bilalsir@yandex.ru>
2022-10-20 15:47:02 +0600

    changed project configurations

--- backend/settings.py
+++ backend/settings.py
@@ -139,6 +139,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/build/static'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
